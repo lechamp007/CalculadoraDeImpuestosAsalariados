169558f92fe7ba06d35e893c0b403c2694750cbc
john Doe <jeronimo.roldan.cardo@gmail.com>
2026-05-13 09:28:18 -0500

    Fix:Nombre de controller

--- src/controller/calculadora_controller.py
+++ src/controller/calculadora_controller.py
@@ -5,7 +5,7 @@ import secret_config
 import psycopg2
 import datetime
 
-class CacluladoraController: 
+class CalculadoraController: 
     def obtener_cursor( ):
         #Conectar a la BD 
         connection = psycopg2.connect( database=secret_config.PGDATABASE, user=secret_config.PGHOST, password=secret_config.PGPASSWORD, host=secret_config.PGPORT)
@@ -13,12 +13,16 @@ class CacluladoraController:
         cursor = connection.cursor()
         return cursor
     def insertar(calculador: Calculadora):
-        cursor= CacluladoraController.obtener_cursor()
+        cursor= CalculadoraController.obtener_cursor()
         #Armar la instruccion SQL 
-        consulta="""INSERT INTO calculadora """
+        consulta="""INSERT INTO calculadora(id,ingresos_anuales,deducciones_generales,
+                    aporte_pension,aporte_salud,numero_dependientes,tiene_vivienda_propia,intereses_credito_vivienda) """
         #Ejecutar el SQL 
         cursor.execute(consulta)
         #Invoque a commit siempre que use una instruccion que modifica en la base de datos
         cursor.connection.commit()
-    def buscar 
+    def buscar(id): 
+        cursor =CalculadoraController.obtener_cursor()
+        consulta="""SELECTid,ingresos_anuales,deducciones_generales,aporte_pension,aporte_salud,
+                    numero_dependientes,tiene_vivienda_propia,intereses_credito_vivienda """
     
