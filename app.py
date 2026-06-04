947a05fcd4fe6282b62a9ad7e7fe2968acf595d3
Your Name <you@example.com>
2026-06-03 19:15:59 -0500

    app.py arreglado

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
-# Para las aplicaciones web creadas con Flask, debemos importar siempre el modulo flask
-# la clase request permite acceso a la información de la petición HTTP
+
 from flask import Flask, request, jsonify , url_for   
 
-# Para poder servir plantillas HTML desde archivos, es necesario importar el modulo render_template
 from flask import render_template
 
 import sys
 sys.path.append("src")
 
-# IMportamos el modulo donde esta creado nuestro Blueprint
+
 from view.web import vista_calculadora
 
-# Flask constructor: crea una variable que nos servirá para comunicarle a Flask
-# la configuración que queremos para nuestra aplicación
+
 app = Flask(__name__)     
 
-# Registramos los Blueprints que creamos 
+ 
 app.register_blueprint( vista_calculadora.blueprint )
 
-# Esta linea permite que nuestra aplicación se ejecute individualmente
+
 if __name__=='__main__':
    app.run( )
